3d05b77169c82580d0f7437c5f82c2916b9bac18
Vasileniuc Alexandru <deepmikoto@gmail.com>
2017-11-10 14:01:35 +0200

    Add files via upload

---
+++ renaming.php
@@ -0,0 +1,42 @@
+<?php
+
+$mapping = [];
+
+$file = fopen('ean_parentid.csv', 'r');
+while (($line = fgetcsv($file, null, ';')) !== false) {
+    $mapping['ean_' . $line[0]] = $line[1];
+}
+
+fclose($file);
+
+$inputFiles = scandir('input/', 1);
+chmod('output', 0777);
+
+$outputFiles = scandir('output/');
+foreach ($outputFiles as $outputFile) {
+    if (!is_file($outputFile)) {
+        continue;
+    }
+    unlink('output/' . $outputFile);
+}
+
+chmod('output', 0777);
+
+foreach ($inputFiles as $inputFile) {
+    $fileEan = reset(explode('_', $inputFile));
+    $fileExt = explode('.', $inputFile);
+    $fileExt = $fileExt[count($fileExt) - 1];
+    if (array_key_exists('ean_' . $fileEan, $mapping)) {
+        $newFileName = $mapping['ean_' . $fileEan] . '-' . (false !== strpos($inputFile, 'Front') ? '2' : '3') . '.' . $fileExt;
+
+        $originalFile = file_get_contents('input/' . $inputFile);
+
+        copy('input/' . $inputFile, 'output/' . $newFileName);
+
+
+    } else {
+        echo 'No mapping found for ean: ' . $fileEan . PHP_EOL;
+    }
+}
+
+echo 'Done';
